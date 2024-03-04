5835e3fe1177c3f9bfb4c3e87c3135e8bdfc25dc
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-03-04 09:51:33 +0200

    Update Enrollee.cs

--- Theme18Pr4/Enrollee.cs
+++ Theme18Pr4/Enrollee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,22 @@ namespace Theme18Pr4
 {
     class Enrollee : Person
     {
-        public Enrollee(string name, string surname, DateTime dt, string faculty) : base (name,surname,dt,faculty) { }
-        public override int Age()
-        {
-            int age = DateTime.Now.Year - Dt.Year;
-            age = (Dt.Month < DateTime.Now.Month) ? age-- : age;
-            return age;
-        }
+        public Enrollee(string name, string surname, DateTime dt, string faculty) : base(name, surname, dt, faculty) { }
         public override void Output()
         {
-            Console.WriteLine($"Имя - {Name}\nФамилия - {Surname}\nДата рождения{Dt}\nФакультет - {Faculty}\nВозраст - {Age()}");
+            Console.WriteLine($"Абитуриент:\nИмя - {Name}\nФамилия - {Surname}\nДата рождения{Dt}\nФакультет - {Faculty}\nВозраст - {Age()}");
+        }
+        public static Enrollee Input()
+        {
+            Console.Write($"Введите имя абитуриента ");
+            string name = Console.ReadLine();
+            Console.Write($"Введите фамилию абитуриента ");
+            string surname = Console.ReadLine();
+            Console.Write($"Введите дату рождения ");
+            DateTime dt = DateTime.Parse(Console.ReadLine());
+            Console.Write($"Введите факультет абитуриента ");
+            string faculty = Console.ReadLine();
+            return new Enrollee(name, surname, dt, faculty);
         }
     }
 }
