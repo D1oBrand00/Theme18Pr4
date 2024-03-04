99d9def39282f7c0bf60ff1aae207bdf40d358ea
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-03-04 09:50:43 +0200

    Update Program.cs

--- Theme18Pr4/Program.cs
+++ Theme18Pr4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,49 +14,74 @@ namespace Theme18Pr4
             int n = int.Parse(Console.ReadLine());
             List<Person> pers = new List<Person>();
             bool b = true;
-            while(b)
+            while (b)
             {
-                Console.WriteLine($"Добавить человека - add");
-                    string person = Console.ReadLine();
-                bool x = true;
+                Console.WriteLine($"Добавить человека - add,show - показать список,search - поиск людей в возрасте от диапозона,q - выйти");
+                string person = Console.ReadLine();
+                int x = 0; ;
                 switch (person)
                 {
-                    
+
                     case "add":
-                        while(x)
+                        Console.WriteLine();
+                        while (x < n)
                         {
-                            Console.WriteLine("Выберите человека (Абитуриент,Студент,Преподаватель");
+                            Console.WriteLine("Выберите человека (Абитуриент,Студент,Преподаватель)");
                             string man = Console.ReadLine();
-                            switch(man.ToLower())
+                            switch (man.ToLower())
                             {
                                 case "абитуриент":
-                                    pers.Add(new Enrollee("Леонид", "Войнован", new DateTime(2008, 05, 10), "исскуств"));
-                                    
+                                    pers.Add(Enrollee.Input());
+                                    x++;
+
                                     break;
                                 case "студент":
-                                    pers.Add(new Student("Даниил", "Полозюк", new DateTime(2006, 03, 20), "юридический",2));
+                                    pers.Add(Student.Input());
+                                    x++;
+                                    Console.WriteLine();
                                     break;
                                 case "преподаватель":
-                                    pers.Add(new Teacher("Сергей", "Зинченко", new DateTime(2000, 01, 23), "информатики","преподаватель", 3));
+                                    pers.Add(Teacher.Input());
+                                    x++;
                                     break;
                                 case "q":
-                                    x = false;
+
                                     break;
                             }
                         }
-                        x = true;
                         break;
                     case "show":
+                        Console.WriteLine();
                         foreach (var elem in pers)
                         {
                             elem.Output();
                         }
                         break;
+                    case "search":
+                        Console.WriteLine();
+                        Console.WriteLine("Введите начало диапозона возраста");
+                        int begin = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите конец диапозона возраста");
+                        int end = int.Parse(Console.ReadLine());
+                        Console.Write($"Люди в возросте с диапозоно от {begin} до {end}\n");
+                        foreach (var elem in pers)
+                        {
+                            if (elem.Age() >= begin && elem.Age() <= end)
+                            {
+                                elem.Output();
+                            }
+                        }
+                        break;
+                    case "q":
+                        b = false;
+                        break;
+                    default:
+                        Console.WriteLine("Я не знаю такой команды :<");
+                        break;
 
                 }
-                b = false;
             }
-            
+
         }
     }
 }
